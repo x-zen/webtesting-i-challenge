2a316dd2973716611b1d8616392e6c8b9dfab414
x-zen <jdharris.x@gmail.com>
2019-05-22 05:56:51 -0500

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,35 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  //increase enhancement by 1 if less than 20
+  if (item < 20) {
+    return item + 1;
+  } else {
+    return item;
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item < 15) {
+    return item - 5;
+  }
+  else if (item >= 15) {
+    if (item > 16) {
+      return item - 1;
+    } else {
+      return item - 10;
+    }
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  if (item < 100) {
+    const needs = 100 - item;
+
+    return item + needs;
+  } else {
+    return item;
+  }
 }
 
 function get(item) {
